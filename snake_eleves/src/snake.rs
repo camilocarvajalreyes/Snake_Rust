78c124d4889df3e8ec049b348b034521b0cb9141
camilocarvajalreyes <camilo.carvajal3009@gmail.com>
2020-05-20 11:48:59 +0200

    snake updated

--- snake_eleves/src/snake.rs
+++ snake_eleves/src/snake.rs
@@ -1,10 +1,11 @@
 // Structure du serpent
-use crate::game::*; // importing structures and objects from game.rs (?)
+use crate::game::*; 
+//pub mod game;// importing structures and objects from game.rs (?)
 use std::collections::VecDeque;
 
 pub struct Snake { //construction of the strut snake
-    body: VecDeque<Point>, //body of the snake as a list of Points
-    direction: Dir, //direction 0 if up, 1 left, 2, down, 3 right (?)
+    pub body: VecDeque<Point>, //body of the snake as a list of Points
+    pub direction: Dir, //direction 0 if up, 1 left, 2, down, 3 right (?)
 }
 impl Snake {
     pub fn new(initialPoint: Point) -> Self { 
@@ -17,8 +18,8 @@ impl Snake {
        then deletes the element in the front (tail) 
     */
     pub fn forward(&mut self){
-        let head = self.body.last();
-        //there will be provlems since .last() returns a type Option object
+        let head = self.body.back().unwrap();
+        //there will be provlems since .back() returns a type Option object
         let newPoint = head.go(self.direction); //see move function for point in game.rs
         self.body.push_back(newPoint);
         self.body.pop_front();
@@ -28,7 +29,7 @@ impl Snake {
     to be called whenever a snake is about to encounter an apple*/
     pub fn grow(&mut self){ 
         // we will use point.move for this as well
-        let head = self.body.last();
+        let head = self.body.back().unwrap();
         let newPoint = head.go(self.direction);
         self.body.push_back(newPoint);
     }
