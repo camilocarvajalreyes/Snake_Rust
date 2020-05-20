9ec8b88157522a1ed48d771d0bc086d55c7fd4df
camilocarvajalreyes <camilo.carvajal3009@gmail.com>
2020-05-20 12:53:00 +0200

    finished snake struct including turn, grow and forward

--- snake_eleves/src/snake.rs
+++ snake_eleves/src/snake.rs
@@ -5,35 +5,45 @@ use std::collections::VecDeque;
 
 pub struct Snake { //construction of the strut snake
     pub body: VecDeque<Point>, //body of the snake as a list of Points
-    pub direction: Dir, //direction 0 if up, 1 left, 2, down, 3 right (?)
+    pub direction: Dir, 
 }
 impl Snake {
-    pub fn new(initialPoint: Point) -> Self { 
+    pub fn new(initial_point: Point) -> Self { 
         let mut vec = VecDeque::new();
-        vec.push_back(initialPoint);
+        vec.push_back(initial_point);
         Snake {body: vec, direction: Dir::UP} //function that returns a single point snake going up as default
     }
 
     /* adds a point in the actual direction to the back (head)
        then deletes the element in the front (tail) 
     */
-    /*
     pub fn forward(&mut self){
         let head = self.body.back().unwrap();
         //there will be provlems since .back() returns a type Option object
-        let newPoint = head.go(self.direction); //see move function for point in game.rs
-        self.body.push_back(newPoint);
+        let new_point = head.go(self.direction); //see move function for point in game.rs
+        self.body.push_back(new_point);
         self.body.pop_front();
         // we need testing for this
     }
     
     /* It adds a point on the head of the snake without getting rid of another point
-    to be called whenever a snake is about to encounter an apple*/
+    to be called whenever a snake is about to encounter an apple
+    Remember that the snake must be pointing towards the apple*/
     pub fn grow(&mut self){ 
         // we will use point.move for this as well
         let head = self.body.back().unwrap();
-        let newPoint = head.go(self.direction);
-        self.body.push_back(newPoint);
+        let new_point = head.go(self.direction);
+        self.body.push_back(new_point);
     }
-    */
+    /* It changes the direction of the snake without moving any of its points
+    This function must be called when turning the snake and it happens within two forwards */
+    pub fn turn(&mut self, dir: Dir){
+        //self.direction = Dir::dir;
+        match dir {
+            Dir::RIGHT => self.direction = Dir::RIGHT,
+            Dir::LEFT => self.direction = Dir::LEFT,
+            Dir::UP => self.direction = Dir::UP,
+            Dir::DOWN => self.direction = Dir::DOWN,
+        }
+    }
 }
