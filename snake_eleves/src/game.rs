8ab775c481d094db476277960232f380c8d0cb9f
Artur Vianna <artur.de-almeida-vianna@student-cs.fr>
2020-05-20 10:22:53 +0200

    Correcting compilation errors

--- snake_eleves/src/game.rs
+++ snake_eleves/src/game.rs
@@ -31,21 +31,30 @@ pub struct Point {
     pub y: u16,
 }
 
+// Une strcture de direction haut niveau
+#[derive(PartialEq)]
+pub enum Dir {
+    UP,
+    LEFT,
+    DOWN,
+    RIGHT,
+}
+
 impl Point {
     pub fn new(x: u16, y: u16) -> Self {
         Point { x, y }
     }
-    pub fn move(&self, direction : i32) -> Point { //returns another point moved in the specific direction
+    pub fn go(&self, direction: Dir) -> Point { //returns another point moved in the specific direction, maybe change name
         //direction 0 if up, 1 left, 2, down, 3 right
         let mut xcor = self.x;
         let mut ycor = self.y;
-        if direction==0 { //move up
+        if direction == Dir::UP { //move up
             ycor = ycor +1;
-        } else if direction==1 { //move left
+        } else if direction == Dir::LEFT { //move left
             xcor = xcor - 1;
-        } else if direction==2 { //moving down
+        } else if direction == Dir::DOWN { //moving down
             ycor = ycor - 1;
-        } else if direction==3 { //move right
+        } else if direction == Dir::RIGHT { //move right
             xcor = xcor + 1;
         }
         Point::new(xcor, ycor) //we must verify if it works!
@@ -142,11 +151,12 @@ pub fn init_game() -> Game {
     // Voir la documentation de _termion__ pour plus d'informations
     let stdout = stdout().into_raw_mode().unwrap();
     let stdin = async_stdin();
+    let initial_point = Point::new(0, 0);
 
     let game = Game {
         stdout: stdout,
         stdin: stdin,
-        snake: Snake {}, // to do
+        snake: Snake::new(initial_point), // to do
         food: generate_food(),
         speed: SPEED,
         field: init_field(),
--- snake_eleves/src/snake.rs
+++ snake_eleves/src/snake.rs
@@ -1,24 +1,29 @@
 // Structure du serpent
 use crate::game::*; // importing structures and objects from game.rs (?)
 use std::collections::VecDeque;
+
 pub struct Snake { //construction of the strut snake
     body: VecDeque<Point>, //body of the snake as a list of Points
-    direction: i32, //direction 0 if up, 1 left, 2, down, 3 right (?)
+    direction: Dir, //direction 0 if up, 1 left, 2, down, 3 right (?)
 }
 impl Snake {
-    pub fn newSnake(initialPoint: Point) -> Self { 
+    pub fn new(initialPoint: Point) -> Self { 
         let mut vec = VecDeque::new();
         vec.push_back(initialPoint);
-        Snake {vec, 0} //function that returns a single point snake going up as default
+        Snake {body: vec, direction: Dir::UP} //function that returns a single point snake going up as default
     }
-    pub fn forward(&mut self){ //adds a point in the actual direction to the back (head)
-        // then deletes the element in the front (tail)
+
+    /* adds a point in the actual direction to the back (head)
+       then deletes the element in the front (tail) 
+    */
+    pub fn forward(&mut self){
         let head = self.body.last();
         //there will be provlems since .last() returns a type Option object
-        let newPoint = head.move(self.direction); //see move function for point in game.rs
+        let newPoint = head.go(self.direction); //see move function for point in game.rs
         self.body.push_back(newPoint);
         self.body.pop_front();
         // we need testing for this
     }
+    
     pub fn grow(){} // we will use point.move for this as well
 }
