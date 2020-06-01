df3a9026942a9403fc57fa77184a024a87922d7e
camilocarvajalreyes <camilo.carvajal3009@gmail.com>
2020-06-01 14:31:30 +0200

    user interaction for choosing playing mode complete

--- snake_eleves/src/main.rs
+++ snake_eleves/src/main.rs
@@ -8,21 +8,55 @@ use std::thread;
 use std::net::{TcpListener, TcpStream, Shutdown};
 use std::io::{Read, Write};
 use std::io;
+use std::thread::sleep;
+use std::time::Duration;
+const PAUSE: u64 = 2000;
 
+fn instructions(input: String) -> i8 {
+    /* Instructions receives the user input and delivers the instructions
+    It also shows an error message if the input is invalid*/
+    let mut successful: i8 = -1;
+    if input.chars().nth(0)==Some('1') {
+        println!("You have chosen single mode! Life is a competition not with others, but with ourselves...");
+        println!("Move your snake with: u for going up, j for going down, h left and k right!");
+        sleep(Duration::from_millis(PAUSE));
+        successful = 0;
+    } else if input.chars().nth(0)==Some('2'){ 
+        println!("Okay for two players! No wise combatant underestimates their antagonist...");
+        println!("Player 1: move your snake with u for going up, j for going down, h left and k right!");
+        sleep(Duration::from_millis(PAUSE));
+        println!("Player 2: move your snake with d for going up, c for going down, x left and v right!");
+        sleep(Duration::from_millis(PAUSE));
+        successful = 0;
+    } else {
+        println!("Pardon? Pick a number between 0 and 1 and start again :)");
+    }
+    successful
+}
 
 fn main() {
+    //source : https://www.youtube.com/watch?v=07pDD0uLjYc
     let mut input = String::new();
-    println!("How many players do you want?:");
+    println!("How many players do you want? (2 maximum):");
     match io::stdin().read_line(&mut input) {
         Ok(_) => {
-            println!("All right then, let's play!");
+            let success = instructions(input);
+            if success == 0 {
+                println!("All right then, let's play!");
+                sleep(Duration::from_millis(PAUSE));
+                let mut game = game::init_game();
+                game.play();
+                // Place le curseur tout en bas
+                write!(game.stdout, "{}", cursor::Goto(1, (game::HEIGHT+2) as u16)).unwrap();
+            }
+            else {println!("Oops, that went wrong somehow...");}
         }
         Err(e) => {
             println!("Sorry, something went wrong: {}", e);
         }
     }
-    let mut game = game::init_game();
+    /*let mut game = game::init_game();
     game.play();
     // Place le curseur tout en bas
-    write!(game.stdout, "{}", cursor::Goto(1, (game::HEIGHT+2) as u16)).unwrap();
+    write!(game.stdout, "{}", cursor::Goto(1, (game::HEIGHT+2) as u16)).unwrap();*/
 }
