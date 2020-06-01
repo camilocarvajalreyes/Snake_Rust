a427dd184dc1cab0c6992f396ef03365f321ccb9
camilocarvajalreyes <camilo.carvajal3009@gmail.com>
2020-06-01 11:05:31 +0200

    reading user input before game

--- snake_eleves/src/main.rs
+++ snake_eleves/src/main.rs
@@ -7,9 +7,20 @@ use termion::cursor;
 use std::thread;
 use std::net::{TcpListener, TcpStream, Shutdown};
 use std::io::{Read, Write};
+use std::io;
 
 
 fn main() {
+    let mut input = String::new();
+    println!("How many players do you want?:");
+    match io::stdin().read_line(&mut input) {
+        Ok(_) => {
+            println!("All right then, let's play!");
+        }
+        Err(e) => {
+            println!("Sorry, something went wrong: {}", e);
+        }
+    }
     let mut game = game::init_game();
     game.play();
     // Place le curseur tout en bas
